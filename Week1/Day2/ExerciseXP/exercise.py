32353ed34a7cfcf673ee032521dbc303ffb0eb71
Aleksei Shapovalov <aleksei.a.shapovalov@gmail.com>
2025-11-06 12:52:28 +0200

    feat: add exercise 1-3

---
+++ Week1/Day2/ExerciseXP/exercise.py
@@ -0,0 +1,27 @@
+#🌟 Exercise 1: Favorite Numbers
+my_fav_numbers = {4, 7, 28}
+my_fav_numbers.add(14)
+my_fav_numbers.add(77)
+my_fav_numbers.remove(77)
+friend_fav_numbers = {1, 6, 4}
+our_fav_numbers = my_fav_numbers | friend_fav_numbers
+print(our_fav_numbers)
+
+#🌟 Exercise 2: Tuple
+tuple_example = (1, 2, 3)
+list_new = list(tuple_example)
+list_new.append(4)
+tuple_new = tuple(list_new)
+print(tuple_new)
+
+#🌟 Exercise 3: List Manipulation
+basket = ["Banana", "Apples", "Oranges", "Blueberries"]
+basket.remove("Banana")
+basket.remove("Blueberries")
+basket.append("Kiwi")
+basket.insert(0, "Apples")
+basket.count("Apples")
+basket.clear()
+print(basket)
+
+#🌟 Exercise 4: Floats
