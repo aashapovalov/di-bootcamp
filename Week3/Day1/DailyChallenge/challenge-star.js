71364c740642284692c9124e48794ded34e51b7a
Aleksei Shapovalov <aleksei.a.shapovalov@gmail.com>
2025-11-23 10:09:08 +0200

    feat: add week4 day1 daily star challenge

---
+++ Week3/Day1/DailyChallenge/challenge-star.js
@@ -0,0 +1,13 @@
+let stringStar = ""
+for(let i = 1; i <= 6; i++) {
+  stringStar += "* "
+  console.log(stringStar)
+}
+
+for (let i = 1; i <= 6; i++) {
+  let stringStar1 = ""
+  for (let j = 1; j <= i; j++) {
+    stringStar1 += "* "
+  }
+  console.log(stringStar1)
+}
