59da7cb632681c212c56391d2187de221729ed4f
Aleksei Shapovalov <aleksei.a.shapovalov@gmail.com>
2025-11-25 18:11:37 +0200

    fet: add doneTask function for challenge for week3 day5

--- Week3/Day5/DailyChallenge/challenge.js
+++ Week3/Day5/DailyChallenge/challenge.js
@@ -1,65 +1,68 @@
 const tasks = [];
-const taskObj = {}
 let submitBtn = document.getElementById('submit-btn');
 let form = document.forms[0]
 let allTasksElement = document.getElementsByClassName('listTasks')[0];
-console.log(form.elements.task.value)
+let taskID = 0;
+let done = false;
 
 function addTask(event) {
   //accessing new task name
   event.preventDefault();
-  let newTask = form.elements.task.value;
-  if (!newTask) {
+  let text = form.elements.task.value;
+  if (!text) {
     alert("Please enter a task");
   }
-  tasks.push(newTask);
+  taskID++;
+  tasks.push({taskID, text, done});
 
-  //add X-icon
+  //add X-icon and event listener
   const icon = document.createElement("i");
   icon.classList.add("icon");
   icon.classList.add("fa-solid", "fa-xmark")
-  icon.addEventListener('click', removeTask)
+  icon.addEventListener('click', deleteTask)
 
-  //add checkbox
+  //add checkbox and event listener
   const inputElement = document.createElement("input");
   inputElement.type = "checkbox";
+  inputElement.addEventListener("change", doneTask)
 
   //add task label
   const taskLabelElement = document.createElement("label");
   taskLabelElement.setAttribute("for", inputElement.id);
-  taskLabelElement.textContent = newTask;
+  taskLabelElement.textContent = text;
 
   //add task container and appending all elements
   const newTaskElement = document.createElement("div");
-  newTaskElement.id = generateID(newTask);
+  newTaskElement.classList.add("task");
+  newTaskElement.id = taskID.toString();
   newTaskElement.appendChild(icon);
   newTaskElement.appendChild(inputElement);
   newTaskElement.appendChild(taskLabelElement);
   allTasksElement.appendChild(newTaskElement);
 }
 
-//generate unique ID and write it to task object
-function generateID(str) {
-  let state = true;
-  let newId = '';
-  while (state === true) {
-    newId = (Math.floor(Math.random() * 1000)).toString();
-    if (Object.keys(taskObj).indexOf(newId) === -1) {
-      state = false;
-    }
-  }
-  taskObj[newId] = str;
-  return newId;
-}
-
 //remove a task
-function removeTask(event) {
+function deleteTask(event) {
   event.preventDefault();
   let parentEl = event.target.parentElement;
   allTasksElement.removeChild(parentEl);
-  const i = tasks.indexOf(taskObj[parentEl.id])
-  tasks.splice(i, 1);
-  delete taskObj[parentEl.id];
+  for (let task of tasks) {
+    if (task.taskID.toString() === parentEl.id) {
+      tasks.splice(tasks.indexOf(task), 1);
+      return
+    }
+  }
+}
+
+//change status of a task
+function doneTask(event) {
+  let parentEl = event.target.closest(".task");
+  for (let task of tasks) {
+    if (task.taskID.toString() === parentEl.id) {
+      task.done = !task.done;
+      return
+    }
+  }
 }
 
 submitBtn.addEventListener('click', addTask);
