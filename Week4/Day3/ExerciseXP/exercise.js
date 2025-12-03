0ed7e61471d0e10e7855662e102a1e2c620c55c5
Aleksei Shapovalov <aleksei.a.shapovalov@gmail.com>
2025-12-03 08:33:58 +0200

    fix: fix week4 day3 exercise

--- Week4/Day3/ExerciseXP/exercise.js
+++ Week4/Day3/ExerciseXP/exercise.js
@@ -4,42 +4,41 @@
 //     the promise resolves if the argument is less than or equal to 10
 // the promise rejects if argument is greater than 10.
 async function compareToTen(num) {
-  try {
-    if (num <= 10) {
-      return "Number is less than 10"
-    } else {
-      throw new Error("Number is greater than 10");
-    }
+  if (num <= 10) {
+    return "Number is less than 10";
+  } else {
+    throw new Error("Number is greater than 10");
   }
-    catch(error) {
-    console.log(error);    }
 }
 compareToTen(2)
-console.log(compareToTen(2));
-console.log(compareToTen(13));
+    .then(res => console.log(res))
+    .catch(err => console.log(err.message));
+
+compareToTen(13)
+    .then(res => console.log(res))
+    .catch(err => console.log(err.message));
 
 // 🌟 Exercise 2 : Promises
 // Instructions
 // Create a promise that resolves itself in 4 seconds and returns a “success” string.
 
-async function promises() {
-  try {
-    setTimeout(()=>{return "Success"},4000)
-  } catch(error) {
-    console.log(error);
-  }
+function promises() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve("Success");
+    }, 4000);
+  });
 }
+
+promises().then(msg => console.log(msg));
 
 // 🌟 Exercise 3 : Resolve & Reject
 // Instructions
 // Use Promise.resolve(value) to create a promise that will resolve itself with a value of 3.
 // Use Promise.reject(error) to create a promise that will reject itself with the string “Boo!”
 
-async function resolveReject() {
-  try {
-    return 3;
-  }
-  catch(error) {
-    console.log("Boo!")
-  }
-}
+const p1 = Promise.resolve(3);
+p1.then(value => console.log(value));
+
+const p2 = Promise.reject("Boo!");
+p2.catch(err => console.log(err));
