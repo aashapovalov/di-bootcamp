0800c578bc5b47b1f422997b27763f9e39674768
Aleksei Shapovalov <aleksei.a.shapovalov@gmail.com>
2025-12-19 19:51:13 +0200

    feat: create static layout, constant and postUser function to post username to server

---
+++ Week6/Day5/DailyChallenge/constants.js
@@ -0,0 +1,22 @@
+export const emojis = [
+  {
+    emoji: "😀",
+    name: "grinning face",
+  },
+  {
+    emoji: "😂",
+    name: "face with tears of joy",
+  },
+  {
+    emoji: "❤️",
+    name: "red heart",
+  },
+  {
+    emoji: "🔥",
+    name: "fire",
+  },
+  {
+    emoji: "🚀",
+    name: "rocket",
+  },
+];
---
+++ Week6/Day5/DailyChallenge/main.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+
+const userForm = document.querySelector("#login-form");
+const errorElement = document.getElementById("login-error");
+
+async function postUser(event) {
+  event.preventDefault();
+  const formData = new FormData(userForm);
+  const username = formData.get("username");
+  if (!username) {
+    errorElement.textContent = "Username is required!"
+    return;
+  }
+  const userJSON = JSON.stringify({username: username});
+  try {
+    const resolve = await axios.post("/users", userJSON);
+    return resolve.data;
+  } catch (error) {
+    errorElement.textContent = error.message;
+  }
+}
