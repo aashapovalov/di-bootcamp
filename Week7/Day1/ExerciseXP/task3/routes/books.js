182d9e51cccbf71551b732846a81fc4c12412e0e
Aleksei Shapovalov <aleksei.a.shapovalov@gmail.com>
2025-12-22 17:19:29 +0200

    fix: fixweek7 day1 exercise

--- Week7/Day1/ExerciseXP/task3/routes/books.js
+++ Week7/Day1/ExerciseXP/task3/routes/books.js
@@ -4,31 +4,31 @@ const books = [];
 
 export const router = express.Router();
 router.get('/books', (req, res) => {
-  res.status(201).send(books);
+  res.status(200).send(books);
 })
 router.post('/books', (req, res) => {
-  const bookTitle = req.body.todoName;
+  const bookTitle = req.body.bookTitle;
   const bookID = crypto.randomUUID();
   const bookEntry = {bookTitle, bookID};
   books.push(bookEntry);
   res.send(bookEntry);
 })
 router.put('/books/:id', (req, res) => {
-  const id = req.params.id;
-  const title = req.body.todoName;
-  const bookEntry = books.find((entry) => entry.bookID === id);
+  const bookID = req.params.id;
+  const title = req.body.bookTitle;
+  const bookEntry = books.find((entry) => entry.bookID === bookID);
   if (!bookEntry) {
-    return res.status(404).send('No todos found.');
+    return res.status(404).send('No books found.');
   }
-  bookEntry.todoName = title;
+  bookEntry.bookTitle = title;
   res.send(bookEntry);
 })
 router.delete('/books/:id', (req, res) => {
-  const id = req.params.id;
-  const bookIndex = books.findIndex((entry) => entry.bookID === id);
+  const bookID = req.params.id;
+  const bookIndex = books.findIndex((entry) => entry.bookID === bookID);
   if (bookIndex >= 0) {
     books.splice(bookIndex, 1);
     return res.send("The task has been deleted.");
   }
-  res.status(404).send('No todos found.');
+  res.status(404).send('No books found.');
 })
