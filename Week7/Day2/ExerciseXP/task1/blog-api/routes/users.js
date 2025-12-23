1b06b1bb4a651eae9ba1fd343e804a4e3b0b7b1e
Aleksei Shapovalov <aleksei.a.shapovalov@gmail.com>
2025-12-23 16:31:37 +0200

    feat: add week7 day2 challenge

--- Week7/Day2/ExerciseXP/task1/blog-api/routes/users.js
+++ Week7/Day2/ExerciseXP/task1/blog-api/routes/users.js
@@ -1,23 +1,29 @@
 import express from "express";
 import {
-  createBlogPost,
-  deleteBlogPost,
-  getBlogPosts,
-  getSingleBlogPost,
-  updateBlogPost
+  hashPassword,
+  createUser,
+  loginUser,
+  getUsers,
+  getSingleUser,
+  updateUser
 } from "../controllers/controllers.js";
 
 export const router = express.Router();
 
-router.get("/", getBlogPosts);
-router.get("/:id", getSingleBlogPost);
-router.post("/", createBlogPost);
-router.put("/:id", updateBlogPost);
-router.delete("/:id", deleteBlogPost);
+router.post("/register", hashPassword, createUser);
+router.post("/login", loginUser);
+router.get("/users", getUsers);
+router.get("/users/:id", getSingleUser);
+router.put("/users/:id", updateUser);
 
 router.use((req, res) => {
   res.status(404).json({
     message: "Route Not Found",
     path: req.originalUrl
   });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({ message: "Server error" });
 });
