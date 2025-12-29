bbf704a69f23e70239636c8d32d8e024e75c062b
Aleksei Shapovalov <aleksei.a.shapovalov@gmail.com>
2025-12-29 14:46:48 +0200

    feat: add week8 day2 exercise gold2

---
+++ Week8/Day2/ExerciseXP/exercise-gold2/src/modal.tsx
@@ -0,0 +1,11 @@
+export function Modal(){
+    return (
+    <>
+        <div className='modal-background'>
+            <div className='modal-body'>
+                <div className='container'></div>
+            </div>
+        </div>
+    </>
+    )
+}
